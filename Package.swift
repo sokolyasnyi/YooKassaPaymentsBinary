ef3bb137935c73c1d66f5494672e9b5127d2465f
Stanislav Sokolov <48655530+sokolyasnyi@users.noreply.github.com>
2024-10-11 15:09:54 +0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
     targets: [
         .binaryTarget(name: "YooKassaPayments",
                       url: "https://github.com/sokolyasnyi/YooKassaPaymentsBinary/releases/download/v7.2.0/YooKassaPayments.xcframework.zip",
-                      checksum: "cb2edf590dd5bdd1c2fc0ada098aecbaab9c219ad6e7725f881659eef7612cc0"),
+                      checksum: "f0c89dfa3a6ef5611012767545795d8b9e90f521b00f7057800b41d2dd38d8bf"),
         .target(
             name: "YooKassaPaymentsBinary"),
         .testTarget(
